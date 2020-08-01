67cd53a3440a22da556e7fb7087939a389de54a3
Emily Vu <evu725@gmail.com>
2020-08-01 18:02:48 -0400

    Fixed storyboard elements and code

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // show keyboard
+        billAmountTextField.becomeFirstResponder()
+        billAmountTextField.isHidden = false
     }
-
+    
+    @IBAction func onTap(_ sender: Any) {
+        }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+    
+    // get inital bill amount and calculate tips
+    let bill = Double(billAmountTextField.text!) ?? 0
+    let tipPercentages = [0.15, 0.18, 0.2]
+    
+    // Calculate tip and total
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+    
+    // Update the tip and total labels
+    tipPercentageLabel.text = String(format: "$%.2f", tip)
+    totalLabel.text = String(format: "$%.2f", total)
+    
+        
+    }
 
 }
 
